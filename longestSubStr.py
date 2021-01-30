52db140113e6f46fa9e86dcd7e09c13a35df4311
Miguel Oyler-Castrillo <33669627+migueloyler@users.noreply.github.com>
2021-01-30 13:07:36 -0500

    Update longestSubStr.py

--- longestSubStr.py
+++ longestSubStr.py
@@ -3,6 +3,9 @@ Given a string s, find the length of the longest substring without repeating cha
 '''
 
 class Solution:
+    
+    #disgusting O(n^2) approach
+    '''
     def lengthOfLongestSubstring(self, s: str) -> int:
         n = len(s)
         if len(s) == 0:
@@ -13,4 +16,30 @@ class Solution:
                 if len(set(s[i:j])) == len(s[i:j]):
                     if len(s[i:j]) > max_len:
                         max_len = len(s[i:j])
+        return max_len
+    '''
+    
+    #beautiful O(n) apprach I figured out after some more thought
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        if len(s) == 1:
+            return 1
+        if len(s) == len(set(s)):
+            return len(s)
+        n = len(s)
+        start_index = 0
+        stop_index = 1
+        max_len = 0
+        while stop_index <= n:
+            substr = s[start_index:stop_index]
+            if len(substr) == len(set(substr)):
+                if len(substr) > max_len:
+                    max_len = len(substr)
+                stop_index+=1
+                continue
+            while len(set(substr)) < len(substr):
+                start_index += 1
+                substr = s[start_index:stop_index]
+                
         return max_len
